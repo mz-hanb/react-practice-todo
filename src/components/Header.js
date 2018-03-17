66c7f1f96d12f984158d82ce830c5d159ce9ef38
mz-hanb <hanb@mz.co.kr>
2018-03-17 10:53:20 +0900

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => (
+  <div className="header">
+    <h1>React To-Do App</h1>
+    <h4>Enhance Your Productivity</h4>
+  </div>
+);
+
+export default Header;
---
+++ src/containers/TodoAppContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from "../components/Header";
+
+class TodoAppContainer extends Component {
+  render() {
+    return (
+      <div className="todoApp">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default TodoAppContainer;
